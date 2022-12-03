3e999f8eb1f3f4ccc3040f8808dcaf4d193cdc96
phathnv <68806946+phathnv@users.noreply.github.com>
2022-12-03 11:35:36 +0700

    Add files via upload

---
+++ gen-map.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+const int HEIGHT = 16;
+const int WIDTH = 6;
+const int MIN_LENGTH = 23;
+const int MAX_LENGTH = 25;
+const int LIMIT = 5000;
+const int MAX_ANS = 10156140; // calculated
+
+#define nextLast(last, dir) (((last << 2) & 15) | dir)
+#define get(last, ind) ((last >> (ind << 1)) & 3)
+
+const int UP = 0;
+const int LEFT = 1;
+const int RIGHT = 2;
+
+int ans, curLen = 0;
+string s = string(HEIGHT * WIDTH * 2, ' ');
+
+string flip(string s) {
+    for (int i = 0; i < HEIGHT; ++i) {
+        int a = 2 * i * WIDTH;
+        int b = 2 * ((i + 1) * WIDTH - 1);
+        while (a < b) {
+            swap(s[a], s[b]);
+            a += 2;
+            b -= 2;
+        }
+    }
+    return s;
+}
+
+void printAns() {
+    if (ans < LIMIT) {
+        cout << s;
+        ++ans;
+    }
+    if (ans < LIMIT) {
+        cout << flip(s);
+        ++ans;
+    }
+}
+
+void gen(int row, int col, int last) {
+    if (ans == LIMIT) {
+        return;
+    }
+    s[(row * WIDTH + col) * 2]++;
+    ++curLen;
+    if (row == HEIGHT - 1) {
+        if (MIN_LENGTH <= curLen && curLen <= MAX_LENGTH) {
+            printAns();
+        }
+        s[(row * WIDTH + col) * 2]--;
+        --curLen;
+        return;
+    }
+    // move up
+    gen(row + 1, col, nextLast(last, UP));
+    // move right
+    if (col + 1 < WIDTH && get(last, 0) != LEFT && get(last, 1) != LEFT) {
+        gen(row, col + 1, nextLast(last, RIGHT));
+    }
+    // move left
+    if (col - 1 >= 0 && get(last, 0) != RIGHT && get(last, 1) != RIGHT) {
+        gen(row, col - 1, nextLast(last, LEFT));
+    }
+    s[(row * WIDTH + col) * 2]--;
+    --curLen;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    s.back() = '\n';
+    for (int i = 0; i < HEIGHT * WIDTH; ++i) {
+        s[2 * i] = '0';
+    }
+
+    cout << min(LIMIT, MAX_ANS) << '\n';
+    for (int i = 0; i < 3; ++i) {
+        s[2 * i]++;
+        curLen = 1;
+        gen(1, i, nextLast(UP, UP));
+        s[2 * i]--;
+        curLen = 0;
+    }
+}
---
+++ validate.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <array>
+#include <set>
+
+using namespace std;
+
+const int HEIGHT = 16;
+const int WIDTH = 6;
+const int MIN_LENGTH = 23;
+const int MAX_LENGTH = 25;
+
+typedef array<array<int, WIDTH>, HEIGHT> gameMap;
+bool valid;
+
+gameMap inputMap() {
+    gameMap m;
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            cin >> m[i][j];
+            if (!(0 <= m[i][j] && m[i][j] <= 1)) {
+                cerr << "Invalid cell code." << endl;
+            }
+        }
+    }
+    return m;
+}
+
+void printMap(const gameMap& m) {
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            cout << m[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+bool checkStartNEndRow(const array<int, WIDTH>& row) {
+    int cnt = 0;
+    for (int i = 0; i < WIDTH; ++i) {
+        cnt += row[i];
+    }
+    return (cnt == 1);
+}
+
+bool checkSquare(const gameMap& m) {
+    for (int i = 0; i < HEIGHT - 1; ++i) {
+        for (int j = 0; j < WIDTH - 1; ++j) {
+            if (m[i][j] + m[i + 1][j] + m[i][j + 1] + m[i + 1][j + 1] == 4) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool validateMap(const gameMap& m) {
+    bool valid = checkStartNEndRow(m[0]) && checkStartNEndRow(m[HEIGHT - 1]) && checkSquare(m);
+    if (!valid) {
+        return false;
+    }
+
+    // check exist path
+    gameMap tmp = m;
+    int x, y, len = 0;
+    for (int j = 0; j < 6; ++j) {
+        if (tmp[0][j] == 1) {
+            len = 1;
+            x = 0;
+            y = j;
+        }
+    }
+    tmp[x][y] = 0;
+
+    while (x != HEIGHT - 1) {
+        if (tmp[x + 1][y] == 1) { // up
+            ++x;
+        } else if (y > 0 && tmp[x][y - 1] == 1) { // left
+            --y;
+        } else if (y + 1 < WIDTH && tmp[x][y + 1] == 1) { // right
+            ++y;
+        } else { // can move
+            cout << "Invalid map." << endl;
+            return false;
+        }
+        ++len;
+        tmp[x][y] = 0;
+    }
+
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            if (tmp[i][j] != 0) { // have branch
+                cout << "Path has branch." << endl;
+                return false;
+            }
+        }
+    }
+
+    return (MIN_LENGTH <= len && len <= MAX_LENGTH); // check length
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int numMap;
+    cin >> numMap;
+    set<gameMap> maps;
+    for (int i = 1; i <= numMap; ++i) {
+        gameMap m = inputMap();
+        if (!validateMap(m)) {
+            continue;
+        }
+        if (maps.count(m)) {
+            cout << "Duplicated map on line " << i << "." << endl;
+            printMap(m);
+        }
+        maps.insert(m);
+    }
+
+    cout << "Number of valid map: " << maps.size() << "/" << numMap << endl;
+}
+
